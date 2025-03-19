16b9cc89cc70d63ca0021ba1d31b485444c995e9
ChromeBridge321 <davidgarcia2809@gmail.com>
2025-03-19 11:42:47 -0600

    purgar

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,6 @@ Route::prefix(('v1'))->middleware('api')->group(function(){
         Route::post('logout',[AuthController::class,'logout'])->name('logout');
         Route::post('refresh',[AuthController::class,'refresh']);
         Route::get('me',[AuthController::class,'me']);
-        Route::get('/purgar', [OrderController::class,'purgar']);
+        Route::get('purgar', [OrderController::class,'purgar']);
     });   
 });
